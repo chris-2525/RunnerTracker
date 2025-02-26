9eade5677ff60ddb797fc79433de1128b684f779
Christine Ang <christineang1@gmail.com>
2025-02-26 15:13:00 -0500

    edited redirect logout page

--- demo/src/main/java/com/example/demo/controllers/LoginController.java
+++ demo/src/main/java/com/example/demo/controllers/LoginController.java
@@ -1,4 +1,36 @@
 package com.example.demo.controllers;
 
-public class Login {
+import com.example.demo.model.LoginProcessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    private final LoginProcessor loginProcessor;
+
+    public LoginController(LoginProcessor loginProcessor) {
+        this.loginProcessor = loginProcessor;
+
+    }
+
+    @GetMapping("/")
+    public String loginGet() {return "login.html";}
+
+    @PostMapping("/")
+    public String loginPost(@RequestParam String username, @RequestParam String password, Model model) {
+        loginProcessor.setUsername(username);
+        loginProcessor.setPassword(password);
+        boolean loggedIn = loginProcessor.login();
+        if (loggedIn) {
+            return "redirect:/runs";
+        }
+        model.addAttribute("message", "Login failed!");
+        return "login.html";
+    }
+
+
 }
